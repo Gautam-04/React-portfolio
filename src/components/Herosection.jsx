9bf5108890f1abf4fe57c073f5d5bfa520510196
Gautam-666 <2022.gautam.rai@ves.ac.in>
2023-06-29 14:32:29 +0530

    Added typeing effects
    Signed-off-by: Gautam-666 <2022.gautam.rai@ves.ac.in>

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -1,4 +1,5 @@
 import Button from "@mui/material/Button";
+import Typewriter from "typewriter-effect";
 
 function HeroSection() {
   return (
@@ -6,12 +7,26 @@ function HeroSection() {
       <div className="w-full md:w-6/12 pl-10 flex flex-col justify-center items-center md:items-start lg:items-center">
         <h1 className="text-2xl md:text-3xl font-bold mb-2">Hi, I am</h1>
         <strong className="text-3xl md:text-4xl font-bold mb-2">
-          Gautam Rai
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("Gautam Rai")
+                .pauseFor(1000)
+                .deleteAll()
+                .typeString("I am React Developer")
+                .start()
+                .deleteAll()
+                .typeString("Gautam Rai")
+                .start();
+            }}
+          />
         </strong>
         <h3 className="text-lg md:text-xl mb-4">I build for the web.</h3>
       </div>
       <div className="w-full md:w-6/12 pl-5 flex flex-col justify-center items-center md:items-center ">
-        <h1 className=" text-2xl md:text-3xl font-bold mb-2">Check My Resume</h1>
+        <h1 className=" text-2xl md:text-3xl font-bold mb-2">
+          Check My Resume
+        </h1>
         <Button variant="outlined" color="primary">
           Resume
         </Button>
